6d484cd3d447d9e32fa23daaa1e2a82b2dd1f440
Elian Ibaj <elianibaj@gmail.com>
2022-08-29 21:06:56 +0200

    dummy handler

--- netlify/functions/sg.js
+++ netlify/functions/sg.js
@@ -1,24 +1,25 @@
 // using Twilio SendGrid's v3 Node.js Library
 // https://github.com/sendgrid/sendgrid-nodejs
-exports.handler = async function () {
-  const sgMail = require('@sendgrid/mail')
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-  const msg = {
-    to: 'elianibaj@gmail.com', // Change to your recipient
-    from: 'elianibaj@gmail.com', // Change to your verified sender
-    subject: 'Sending with SendGrid is Fun',
-    text: 'and easy to do anywhere, even with Node.js',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-  }
-  sgMail
-    .send(msg)
-    .then(() => {
-      console.log('Email sent')
-    })
-    .catch((error) => {
-      console.error(error)
-    })
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
+const msg = {
+  to: 'elianibaj@gmail.com', // Change to your recipient
+  from: 'elianibaj@gmail.com', // Change to your verified sender
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+}
+sgMail
+  .send(msg)
+  .then(() => {
+    console.log('Email sent')
+  })
+  .catch((error) => {
+    console.error(error)
+  })
+
+exports.handler = async function () {
   return {
     statusCode: 200,
     body: '',
